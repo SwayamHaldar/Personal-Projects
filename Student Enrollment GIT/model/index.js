d6df3ac02e2be5f386261fb6204a0fdd83dcd32c
SwayamHaldar <130141881+SwayamHaldar@users.noreply.github.com>
2025-07-18 12:01:06 +0530

    Delete Student Enrollment GIT directory

--- Student Enrollment GIT/model/index.js
+++
@@ -1,7 +0,0 @@
-import Student from "./studentdetails.js";
-import Enrolled from "./Enroll.js";
-
-Student.hasMany(Enrolled, { foreignKey: "StudentID" });
-Enrolled.belongsTo(Student, { foreignKey: "StudentID" });
-
-export { Student, Enrolled };
